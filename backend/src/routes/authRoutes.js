ceecce6591fd224932f1b16231180c943001610a
Saif-Ul-llah <haroonhammad300@gmail.com>
2025-08-23 14:51:51 +0500

    routes compete for AUTH

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -10,7 +10,29 @@ router.all("/signup",(req, res)=>
     res.status(405).json({error:"Method Not Allowed"})
 );
 
-router.post("/login", loginUser);
-router.all("/login",(req, res)=>
+router.post("/signin", loginUser);
+router.all("/signin",(req, res)=>
     res.status(405).json({error:"Method Not Allowed"})
 );
+
+router.post("/verify", loginUser);
+router.all("/verify",(req, res)=>
+    res.status(405).json({error:"Method Not Allowed"})
+);
+
+router.post("resend-otp", loginUser);
+router.all("/resend-otp",(req, res)=>
+    res.status(405).json({error:"Method Not Allowed"})
+);
+
+router.post("/forgot-password", loginUser);
+router.all("/forgot-password",(req, res)=>
+    res.status(405).json({error:"Method Not Allowed"})
+);
+
+router.post("/reset-password", loginUser);
+router.all("/reset-password",(req, res)=>
+    res.status(405).json({error:"Method Not Allowed"})
+);
+
+export default router;
