7622a780912788ea232af0e18d1e0ff3dd30ef9d
Haroon <haroonhammad300@gmail.com>
2025-08-30 20:08:16 +0500

    Update authService.js

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -43,3 +43,62 @@ export const signinUser = async ({email, password}) => {
     
     return {status: "success", message: "User logged in successfully",data: token};
 }
+
+export const verifyUserOtp = async ({email, otp}) => {
+    const user = await User.findOne({email});
+    if (!user) {throw new Error("User not found");}
+
+    if (user.otp !== otp) {throw new Error("Invalid OTP");}
+    if (user.otpExpiry < Date.now()) {throw new Error("OTP expired");}
+
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+    user.otp = null;
+    user.otpExpiry = null;
+    user.save();
+    return {
+        status: "login Successful",
+        token,
+        user: {
+            name: user.name,
+            email: user.email,
+            profilePic: user.profilePic
+        },
+    };
+}
+
+const resendOtp = async ({email}) => {
+    const user = await User.findOne({email});
+    if (!user) {throw new Error("User not found");}
+
+    const otp = generateOtp();
+    const otpExpiry = new Date(Date.now() + 15 * 60 * 1000);
+
+    user.otp = otp;
+    user.otpExpiry = otpExpiry;
+    await user.save();
+
+    await sendEmail(
+        email,
+        "LexEye OTP",
+        `<h2>Hello ${user.name}</h2> <p>Your new OTP is <b>${otp}</b>.</br> This OTP is valid for 15 minutes.</p>`
+    );
+
+    return {message: "New OTP sent successfully to email"};
+}
+
+
+export const findUserById = async (id) => {
+    const user = await User.findById(id);
+    return user;
+}
+
+export const updatePassword = async (id, password) => {
+    const user = await User.findOne({email});
+    if (!user) {throw new Error("User not found");}
+
+    const hashed = await bcrypt.hash(password, 10);
+    user.password = hashed;
+    await user.save();
+    return {message: "Password updated successfully"};
+}
